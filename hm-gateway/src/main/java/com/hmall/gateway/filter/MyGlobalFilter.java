a0b8aee4e69d11ff33b35d03700c1e6ed87423e0
chenjiajieeeee <2868203618@qq.com>
2024-02-11 22:43:55 +0800

    day-01 利用openfeign进行拆分，拆分完毕

---
+++ hm-gateway/src/main/java/com/hmall/gateway/filter/MyGlobalFilter.java
@@ -0,0 +1,26 @@
+package com.hmall.gateway.filter;
+
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+@Component
+public class MyGlobalFilter implements GlobalFilter, Ordered {
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        //TODO 登录校验逻辑
+        final ServerHttpRequest request = exchange.getRequest();
+        final HttpHeaders headers = request.getHeaders();
+        return chain.filter(exchange);
+    }
+
+    @Override
+    public int getOrder() {
+        return 0;
+    }
+}
